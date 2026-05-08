6db5124df8919732eb3429a146d89c079e7f8634
elhadjidiagne002-netizen <elhadjidiagne002@gmail.com>
2026-05-08 12:31:39 +0000

    Fix template literals in message API handler

--- functions/api/messages/index.js
+++ functions/api/messages/index.js
@@ -10,8 +10,8 @@ export async function onRequest({ request, env }) {
 
     if (request.method === 'GET') {
       const withUser = u.searchParams.get('with');
-      let qs = \`or=(from_id.eq.\${user.id},to_id.eq.\${user.id})&deleted=eq.false&order=created_at.asc\`;
-      if (withUser) qs = \`or=(and(from_id.eq.\${user.id},to_id.eq.\${withUser}),and(from_id.eq.\${withUser},to_id.eq.\${user.id}))&deleted=eq.false&order=created_at.asc\`;
+      let qs = `or=(from_id.eq.${user.id},to_id.eq.${user.id})&deleted=eq.false&order=created_at.asc`;
+      if (withUser) qs = `or=(and(from_id.eq.${user.id},to_id.eq.${withUser}),and(from_id.eq.${withUser},to_id.eq.${user.id}))&deleted=eq.false&order=created_at.asc`;
       const data = await sb.from('messages').select('*', qs);
       return json(data || []);
     }
@@ -29,7 +29,7 @@ export async function onRequest({ request, env }) {
       const saved = Array.isArray(data) ? data[0] : data;
       // Notifier le destinataire
       await sb.from('notifications').insert({
-        user_id: body.toId, type: 'info', title: \`Message de \${user.name || 'quelqu\'un'}\`,
+        user_id: body.toId, type: 'info', title: `Message de ${user.name || 'quelqu\'un'}`,
         message: (body.text || '').slice(0, 80),
       }).catch(() => {});
       return json(saved, 201);
